a6022672dba9de63048ecfebb3ba59fb92b2fc5d
Jenny Kim <kimj1905@gmail.com>
2021-04-21 00:15:54 -0400

    commit tree 20888b8f6f36010ef81ac62ce1e13907f8dcb3f6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "nice", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "light brown", "sassy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "sweet", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "spicy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "black", "grouchy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 9, "F", "dark brown", "paranoid", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "M", "light brown", "scared", false);
